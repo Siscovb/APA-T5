5f66e09ca422dbb2e47529af506f4298e636666e
albert-gimenez <78741339+albert-gimenez@users.noreply.github.com>
2023-05-12 16:39:05 +0200

    comit1
    Funcion 1 funciona

--- estereo.py
+++ estereo.py
@@ -1,28 +1,50 @@
+import struct as st
 def estereo2mono (ficEste, ficMono, canal=2):
-    señal = bytearray()
-    fichero = open(ficEste, mode='rb', buffering=0)
-    if canal == 0:
-        i=0
-        while fichero:
-            fichero.seek(i)
-            señal=fichero.read(2)
-            i+=4
-    elif canal == 1:
-        i=2
-        while fichero:
-            fichero.seek(i)
-            señal=fichero.read(2)
-            i+=4
-    with open(ficMono, 'wb') as output_file:
-        output_file.write('\n'.join(map(str, señal)))
-    #elif canal == 2:
-    #    while fichero:
-    #        fichero.seek(i)
-    #        señalL=(float(fichero.read(1)) + float(fichero.read(2)))/2
-    #        i+=4
-    #elif canal == 3:
-    #    while fichero:
-    #        i=0
-    #        fichero.seek(i)
-    #        señal=fichero.read(2)
-    #        i+=4
+
+    with open(ficEste, 'rb') as ficEn:
+        cabecera = '<4sI4s'  
+        buffer = ficEn.read(st.calcsize(cabecera))
+        chunkID, chunksize, formatt = st.unpack(cabecera, buffer)
+
+        formato = '<4sI2H2I2H'         
+        buffer = ficEn.read(st.calcsize(formato)) 
+        (subChunkID, subChunkSize, audioFormat,numChannels, sampleRate, byteRate,blockAlign, BitsxSample) = st.unpack(formato, buffer)
+
+        cabecera = '<4sI'         
+        buffer = ficEn.read(st.calcsize(cabecera))
+        subChunk2ID, subChunk2Size = st.unpack(cabecera, buffer)
+
+        numMuestras = subChunk2Size // blockAlign
+        datos = f'<{numMuestras}h' 
+        buffer = ficEn.read(st.calcsize(datos))
+        Datos = st.unpack(datos, buffer)
+        DatosL=Datos[0::2]
+        DatosR=Datos[1::2]
+        semisuma= [(dl+dr)//2 for dl,dr in zip(DatosL,DatosR)]
+        semiresta= [(dl-dr)//2 for dl,dr in zip(DatosL,DatosR)]
+        cabeceraOut = st.pack('4sI4s4sIhhIIhh4sI', chunkID, (36+subChunk2Size), formatt, subChunkID, subChunkSize,
+                            audioFormat,numChannels, sampleRate, byteRate, blockAlign, BitsxSample, subChunk2ID, subChunk2Size)
+        with open(ficMono, 'wb') as Fsal:
+            Fsal.write(cabeceraOut)
+            if canal == 0:
+                binario = st.pack(f'{len(DatosL)}i', *DatosL)
+                Fsal.write(binario)
+            if canal == 1:
+                binario = st.pack(f'{len(DatosR)}i', *DatosR)
+                Fsal.write(binario)
+            if canal == 2:
+                binario = st.pack(f'{len(semisuma)}i', *semisuma)
+                Fsal.write(binario)
+            if canal == 3:
+                binario = st.pack(f'{len(semiresta)}i', *semiresta)
+                Fsal.write(binario)
+            
+        
+        
+
+
+
+
+        
+
+    
---
+++ waves.py
@@ -0,0 +1,38 @@
+import struct as st
+
+def abreWave(fichero):
+    with open(fichero, 'rb') as fpwave:  #AIXÒ ES UN GESTOR DE FICHERO, EL TANCA SOL.
+        cabecera = '<4sI4s'  #< = portable LittleEndian (Estem seguint dibuixet del format de WAVE), 4s crec q vol dir 4 bytes s(sense signe) (cabecera = lila)
+        
+        #Ara llegim:
+        buffer = fpwave.read(st.calcsize(cabecera))
+
+        #Assignem a variables el que treiem de buffer (passat de binari a str)
+        chunkID, chunksize, formatt = st.unpack(cabecera, buffer)
+        if chunkID != b'RIFF' or formatt != b'WAVE':
+            raise Exception('El fichero NO es formato WAVE') from None  #el from None, es pq no surti en principi tot el rollo
+
+        formato = '<4sI2H2I2H'         #ho fem amb majuscula per si de cas es molt gran o negatiiu'
+        buffer = fpwave.read(st.calcsize(formato)) #Ho lleggiiim
+
+        #Posem un parentesis pq càpiga tot 
+        (subChunkID, subChunkSize, audioFormat,
+         numChannels, sampleRate, byteRate,
+        blockAlign, BitsxSample) = st.unpack(formato, buffer)
+
+        cabecera = '<4sI'         
+        buffer = fpwave.read(st.calcsize(cabecera))
+        subChunk2ID, subChunk2Size = st.unpack(cabecera, buffer)
+
+        numMuestras = subChunk2Size // blockAlign
+        datos = f'<{numMuestras}h'    #entre {} perque serà un numero, es perque substitueixi xx el valor de la variable, la f es pq es fstring
+        buffer = fpwave.read(st.calcsize(datos))
+        Datos = st.unpack(datos, buffer)
+
+
+        return (chunkID, chunksize, formatt, subChunkID, subChunkSize, audioFormat,
+        numChannels, sampleRate, byteRate,
+        blockAlign, BitsxSample, subChunk2ID, subChunk2Size, Datos)
+
+    
+
